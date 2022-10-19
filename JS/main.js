8e7af4e4895bae42f3919ef5e56715d1440e3022
anaramirezgmail <anamicaela0000@gmail.com>
2022-10-18 20:53:22 -0500

    envio decimo Ana

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,3 @@
 document.getElementById("main-header").addEventListener("click",function(){
-    document.body.style.fontSize="15px"; //asi se modifica una propiedad de estilo 
+    document.body.style.fontSize="25px"; //asi se modifica una propiedad de estilo 
 })
